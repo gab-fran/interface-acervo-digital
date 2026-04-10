0dd919e8de5aaee9e800d5833748d71977a1b3c9
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-04-10 16:11:33 -0300

    feat: Implementando serviços de requisição CRUD e componentes de listagem para livros

--- src/pages/PLivros/PLivros.tsx
+++ src/pages/PLivros/PLivros.tsx
@@ -1,14 +1,13 @@
 import type { JSX } from "react";
 import Navegacao from "../../components/Navegacao/Navegacao";
 import Rodape from "../../components/Rodape/Rodape";
+import ListagemLivro from "../../components/Listagem/ListagemLivro/ListagemLivro";
 
 function PLivros(): JSX.Element {
     return (
         <div className="pagina">
             <Navegacao/>
-            <div className="pagina-conteudo">
-                <p>Livros</p>
-            </div>
+            <ListagemLivro />
             <Rodape />
         </div>
     );
