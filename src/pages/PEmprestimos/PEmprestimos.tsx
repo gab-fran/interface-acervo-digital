d1f3ad0c0759e07ad58e35343b2e1b59de74be29
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-04-10 16:12:23 -0300

    feat: Implementando serviços de requisição CRUD e componentes de listagem para emprestimos

--- src/pages/PEmprestimos/PEmprestimos.tsx
+++ src/pages/PEmprestimos/PEmprestimos.tsx
@@ -1,14 +1,13 @@
 import type { JSX } from "react";
 import Navegacao from "../../components/Navegacao/Navegacao";
 import Rodape from "../../components/Rodape/Rodape";
+import ListagemEmprestimo from "../../components/Listagem/ListagemEmprestimo/ListagemEmprestimo";
 
 function PEmprestimos(): JSX.Element {
     return (
         <div className="pagina">
             <Navegacao/>
-            <div className="pagina-conteudo">
-                <p>Emprestimos</p>
-            </div>
+            <ListagemEmprestimo />
             <Rodape />
         </div>
     );
